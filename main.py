35e1c27a0232bf384f28db105988d8e389ab44b4
giulia-cinquegrana <gcatherine.cinquegrana@gmail.com>
2025-08-13 12:57:39 +1000

    euler time

--- main.py
+++ main.py
@@ -25,22 +25,60 @@ class reaction_rates():
         return np.array([dYA_dt, dYB_dt, dYC_dt])
 
     def __init__(self):
-        pass
-
-    def SN(self):
 
         tau = {'ni56': 5.251e5,
-               'co56': 6.672e6,
-               'ni57': 1.28e5,
-               'co57': 2.35e7,
-               'ti44': 1.86e9,
-               'sc44': 1.43e4
-               }
-        
-        Y0  = [1.0, 0.0, 0.0]                    
-        hl  = [tau['ni56'], tau['co56']]      
+        'co56': 6.672e6,
+        'ni57': 1.28e5,
+        'co57': 2.35e7,
+        'ti44': 1.86e9,
+        'sc44': 1.43e4
+        }
 
-        return self.calc_decay(Y0, hl)
-            
+        self.Y = np.array([1.0, 0.0, 0.0])
+        self.t = 0.0
+        self.hl = np.array([tau['ni56'], tau['co56']])
+
+        pass
+    
+    def step(self, dt, adaptive=True, tol_abs=1e-18, tol_rel=0.02):
+        '''
+        atol: pick the smallest abundance change you care about, e.g. 10^-20, 10^-18.
+        '''
+        # go one step
+        # currently euler (p=1)
+        p = 1
+        s = 0.9  # safety factor
+        
+        while True:
+
+            dYdt = self.calc_decay(self.Y, self.hl)
+
+            Y_trial = self.Y + dt * dYdt
+            delta = np.abs(Y_trial - self.Y)
+            scale_i = tol_abs + tol_rel * np.maximum(np.abs(self.Y),np.abs(Y_trial))
+            error_i = np.abs(delta) / scale_i
+            error_max = float(np.max(error_i))
+
+            if adaptive and (error_max > 1):
+                fr = max(0.2, min(2.0, s * error_max**(-1/p)))
+                dt *= fr
+                continue
+
+            # where error_max < 1, accept
+            self.Y = np.clip(Y_trial, 0.0, None)
+            self.t += dt
+
+            if adaptive:
+                # if last step easy (small error_max), grow dt.
+                # if last was tricky (large error_max), shrink dt.
+                # max_growth = increased by factor of 2
+                # min_shrink = halved
+                growth = min(2.0, max(0.5, s / max(error_max, 1e-16))) # max(error_max, 1e-16) = avoid divide by zero if error_max is perfect
+                dt_next = dt * growth
+            else:
+                dt_next = dt
+
+            return dt, dt_next, error_max
+
 r = reaction_rates()
-print(r.SN())
+print(r.step(0.1))
